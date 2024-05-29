210cd2c3557c2e3511a2eecaa8ae5a55513d3fb7
Danil Zizevskiy <159880453+DanilZizevskyi@users.noreply.github.com>
2024-05-29 18:01:14 +0300

    Відреагував основну часнину коду
    Відредагував основну частину коду , видалив зайві дії для більш швидкої роботи программи

--- Polynomials/Polynomials.cpp
+++ Polynomials/Polynomials.cpp
@@ -7,38 +7,6 @@
 
 using namespace std;
 
-// Функція для множення поліномів
-int* Multiply(int* poly1, int* poly2, int deg1, int deg2) {
-    int* result = new int[deg1 + deg2 + 1];
-
-    for (int i = 0; i <= deg1 + deg2; ++i) {
-        result[i] = 0;
-    }
-
-    for (int i = 0; i <= deg1; ++i) {
-        for (int j = 0; j <= deg2; ++j) {
-            result[i + j] += poly1[i] * poly2[j];
-        }
-    }
-
-    return result;
-}
-
-// Функція для множення полінома на одночлен
-int* MultiplyOD(int* poly, int deg, int monomialCoeff, int monomialDegree) {
-    int* result = new int[deg + monomialDegree + 1];
-
-    for (int i = 0; i <= deg + monomialDegree; ++i) {
-        result[i] = 0;
-    }
-
-    for (int i = 0; i <= deg; ++i) {
-        result[i + monomialDegree] = poly[i] * monomialCoeff;
-    }
-
-    return result;
-}
-
 // Функція для виводу полінома
 void Printp(int* poly, int deg) {
     for (int i = deg; i >= 0; --i) {
